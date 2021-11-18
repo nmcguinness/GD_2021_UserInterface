935daf0142432668fa160f6fc813924456aaa8e2
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-18 10:58:36 +0000

    Add project files.

---
+++ GD_2021_UserInterface/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+/*
+
+ */
+
+namespace GD_2021_UserInterface
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont spriteFont;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            spriteFont = Content.Load<SpriteFont>("ui_font");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //How to draw text/image?
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(spriteFont, "Hello",
+                new Vector2(0, 0), Color.White, 0,
+                Vector2.Zero, 1, SpriteEffects.None, 0);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GD_2021_UserInterface/Transform2D.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GD_2021_UserInterface
+{
+    public sealed class Transform2D
+    {
+        public Vector2 localTranslation;
+        public Vector2 localScale;
+        public float rotationInDegrees;
+    }
+}
