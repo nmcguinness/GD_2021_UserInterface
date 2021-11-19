543198f1c15aad7e8df0d22b720ff62e49d7a3ed
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-19 12:38:09 +0000

    Added calls to update and draw

--- GD_2021_UserInterface/UISceneManager.cs
+++ GD_2021_UserInterface/UISceneManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 
 namespace GD_2021_UserInterface
@@ -7,8 +8,15 @@ namespace GD_2021_UserInterface
     public class UIScene
     {
         //id, name, isVisible, isEnabled, UISceneType {Options, Main, InGame}
+        private string id, name;
 
         private List<UIObject> uiObjects;
+
+        public string ID { get => id; set => id = "UIS_" + Guid.NewGuid(); }
+
+        //TODO - add length and null check on name
+        public string Name { get => name; set => name = value.Trim(); }
+
         public List<UIObject> UiObjects { get => uiObjects; set => uiObjects = value; }
 
         //constructor, add, remove, find, clear, update, draw
@@ -30,20 +38,65 @@ namespace GD_2021_UserInterface
     public class UISceneManager : DrawableGameComponent
     {
         private SpriteBatch spriteBatch;
+        private Dictionary<string, UIScene> scenes;
+        private UIScene activeScene;
+        private string activeSceneName;
 
         public UISceneManager(Game game, SpriteBatch spriteBatch) : base(game)
         {
             this.spriteBatch = spriteBatch;
+            scenes = new Dictionary<string, UIScene>();
+            activeScene = null;
+            activeSceneName = "";
+        }
+
+        public bool SetActiveScene(string sceneName)
+        {
+            UIScene scene = Find(sceneName);
+            if (scene != null)
+            {
+                activeScene = scene;
+                activeSceneName = sceneName;
+                return true;
+            }
+            return false;
+        }
+
+        public void Add(UIScene scene)
+        {
+            if (!scenes.ContainsKey(scene.Name))
+                scenes.Add(scene.Name, scene);
+        }
+
+        public bool Remove(string key)
+        {
+            return scenes.Remove(key);
+        }
+
+        public UIScene Find(string key)
+        {
+            UIScene uiScene;
+            scenes.TryGetValue(key, out uiScene);
+            return uiScene;
+        }
+
+        public void Clear()
+        {
+            if (scenes.Count != 0)
+                scenes.Clear();
         }
 
         public override void Update(GameTime gameTime)
         {
-            base.Update(gameTime);
+            activeScene?.Update();
         }
 
         public override void Draw(GameTime gameTime)
         {
-            base.Draw(gameTime);
+            spriteBatch.Begin(SpriteSortMode.BackToFront,
+                BlendState.AlphaBlend, null, null);
+            activeScene?.Draw(spriteBatch);
+            spriteBatch.End();
         }
     }
 }
