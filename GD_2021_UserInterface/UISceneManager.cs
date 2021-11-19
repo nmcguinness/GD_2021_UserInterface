76ae7b5b1a21131b2c542cbdbef8b145369df5e7
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-19 12:11:01 +0000

    Added initial UISceneManager

---
+++ GD_2021_UserInterface/UISceneManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GD_2021_UserInterface
+{
+    public class UISceneManager : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+
+        public UISceneManager(Game game, SpriteBatch spriteBatch) : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
